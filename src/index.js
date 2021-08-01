dd1f3dda5a3c849353149450551b32c3ea2bac5d
jpaverd <jpaverd@comcast.net>
2021-07-31 18:46:30 -0600

    completed project setup

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ const elvenShieldRecipe = {
     ironIngot: 1,
     refinedMoonstone: 4,
 };
+const elvenGauntletsRecipe = {
+    ...elvenShieldRecipe,
+    leather: 1,
+    ironIngot: 1,
+};
+
 console.log(elvenShieldRecipe);
+console.log(elvenGauntletsRecipe);
+
