8d4516e9b424689f30640cbb548c96d6e14e1ac0
jpaverd <jpaverd@comcast.net>
2021-08-07 07:24:47 -0600

    add babel

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # webpack-config
+
+From Swashbuckling with Code's [Webpack 5 full project setup](https://www.youtube.com/watch?v=TOb1c39m64A&list=PLmZPx_9ZF_sB4orswXdpThGMX9ii2uP7Z&index=6 ) tutorial.
+
+[Fixing](https://www.youtube.com/watch?v=t9okUDkRUDc&list=PLmZPx_9ZF_sB4orswXdpThGMX9ii2uP7Z&index=8)  NODE_ENV is not recognized error on Windows.
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,23 @@
+let mode="development";
+
+if (process.env.NODE_ENV === "production") {
+    mode = "production";
+}
 module.exports = {
-    mode: "development",
+    mode: mode,
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    
+    devtool: "source-map",
+
     devServer: {
         contentBase: "./dist",
     },
